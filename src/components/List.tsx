0eeb1ad987af704cbb83e688d53bc568b2c8c171
dima tikhtey <uu36548@gmail.com>
2025-07-15 13:17:55 +0300

    сломал стрелки но картку норм

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,6 +4,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useRef } from 'react';
 import arrowRight from '../assets/arrow-right.svg';
+
 interface ListProps {
   comments: {
     id: number;
@@ -26,7 +27,7 @@ const List = ({ comments }: ListProps) => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 11114000,
-    arrows: false, // 🔴 обязательно отключи встроенные
+    arrows: false,
     responsive: [
       {
         breakpoint: 1280,
@@ -44,24 +45,21 @@ const List = ({ comments }: ListProps) => {
   };
 
   return (
-    <div className="relative w-full mx-auto">
+    <div className="relative max-w-[1200px] mx-auto w-full">
       {/* Кастомные стрелки */}
       <div className="absolute top-1/2 left-0 right-0 z-10 flex justify-between items-center px-4 -translate-y-1/2 pointer-events-none">
         <div
           onClick={() => sliderRef.current?.slickPrev()}
-          className="pointer-events-auto bg-white shadow-md rounded-full p-2 cursor-pointer"
+          className="pointer-events-auto bg-white shadow-md rounded-full w-[60px] h-[60px] flex items-center justify-center cursor-pointer"
         >
-          <button className="w-[68px] h-[68px] rounded-full">
-            <img src={arrowRight} alt="Previous" className="rotate-180" />
-          </button>
+          <img src={arrowRight} alt="Previous" className="rotate-180 w-6 h-6" />
         </div>
+
         <div
           onClick={() => sliderRef.current?.slickNext()}
-          className="pointer-events-auto bg-white shadow-md rounded-full p-2 cursor-pointer"
+          className="pointer-events-auto bg-white shadow-md rounded-full w-[60px] h-[60px] flex items-center justify-center cursor-pointer"
         >
-          <button  className="w-[68px] h-[68px] rounded-full">
-            <img src={arrowRight} alt="Next" />
-          </button>
+          <img src={arrowRight} alt="Next" className="w-6 h-6" />
         </div>
       </div>
 
